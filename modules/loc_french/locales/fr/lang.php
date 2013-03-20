d591dec060477814455489b8ed5a0558f54f32eb
David Montero <david.montero.isusi@gmail.com>
2013-03-20 22:16:15 +0100

    Moved localization files from modules directory to correct location (german translation has several files other locales don't)

--- modules/loc_french/locales/fr/lang.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$dir = basename(dirname(__FILE__));
-	$LANGUAGES['fr_FR'] = array($dir, 'French', 'Francais', 'fra');
-	$LANGUAGES['fr_CA'] = array($dir, 'French (Can)', 'Francais (Can)', 'frc');
-?>
--- modules/loc_spanish/locales/es/lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Language list for supported locales.
-// This is used in the 2.0 version of dotproject to correctly identify Windows language
-// support.  See the English version for details of its format.
-$dir = basename(dirname(__FILE__));
-
-$LANGUAGES['es_SP'] = array( $dir, 'Spanish', 'Español', 'esp', "utf-8");
-$LANGUAGES['es_MX'] = array( $dir, 'Spanish (MX)', 'Español (MX)', 'esm', "utf-8");
-?>
