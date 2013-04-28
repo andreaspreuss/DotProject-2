cc1983e2d45c30a7fe577fccfc7c09e6a016d66e
David Montero <david.montero.isusi@gmail.com>
2013-04-28 16:38:41 +0200

    Removed commented code

--- functions/projects_func.php
+++ functions/projects_func.php
@@ -18,26 +18,4 @@ foreach ($ppriority_color as $key => $val) {
     $priority[$key]['color'] = $val;
 }
 
-/*
-// kept for reference
-$priority = array(
- -1 => array(
- 	'name' => 'low',
- 	'color' => '#E5F7FF'
- 	),
- 0 => array(
- 	'name' => 'normal',
- 	'color' => ''//#CCFFCA
- 	),
- 1 => array(
- 	'name' => 'high',
- 	'color' => '#FFDCB3'
- 	),
- 2 => array(
- 	'name' => 'immediate',
- 	'color' => '#FF887C'
- 	)
-);
-*/
-
 ?>
